66ca9cee502f3dfac9cdcb747a719c8be78e7bf5
saber <sbaerm.com@gmail.com>
2023-06-19 19:17:10 +0330

    add UserRepository

---
+++ src/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package repository;
+
+import Base.repository.BaseRepository;
+import exception.NatCodeExistsException;
+import exception.PhoneExistsException;
+import exception.UsernameExistException;
+import model.User;
+
+import java.sql.SQLException;
+
+public interface UserRepository
+        extends BaseRepository<Integer, User> {
+
+    boolean isExistsUsername(String username) throws SQLException, UsernameExistException;
+
+    boolean isExistsNatCode(String natCode) throws SQLException, NatCodeExistsException;
+
+    boolean isExistsPhone(String phone) throws SQLException, PhoneExistsException;
+
+    User findUserByUsername(String username) throws SQLException;
+
+
+}
