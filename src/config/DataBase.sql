0c5068674870fde7baa189b91baef3e203fcda15
saber <sbaerm.com@gmail.com>
2023-06-19 19:15:25 +0330

    Create database

---
+++ src/config/DataBase.sql
@@ -0,0 +1,12 @@
+create table if not exists user_table
+(
+    id            serial
+        constraint user_table_pk
+            primary key,
+    first_name    varchar default 50,
+    last_name     varchar default 50,
+    username      varchar default 50,
+    password      varchar default 50,
+    national_code varchar default 11,
+    phone         varchar default 12
+);
