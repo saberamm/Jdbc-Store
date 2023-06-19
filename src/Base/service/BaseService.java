4e9c6348ef05d017cd065dfde4ec0576bdfd7da6
saber <sbaerm.com@gmail.com>
2023-06-19 19:10:23 +0330

    Add Base service

---
+++ src/Base/service/BaseService.java
@@ -0,0 +1,18 @@
+package Base.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface BaseService <ID,TYPE>{
+
+    void save(TYPE entity) throws SQLException;
+
+    void delete(ID id) throws SQLException;
+
+    List<TYPE> findAll() throws SQLException;
+
+    TYPE findById(ID id) throws SQLException;
+
+    void update(TYPE entity) throws SQLException;
+}
+
