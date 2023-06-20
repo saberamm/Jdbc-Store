ab62f2ef902b7fd39bc5cae9538155b8710f016d
saber <sbaerm.com@gmail.com>
2023-06-20 13:44:20 +0330

    add shopping cart table

--- src/main/java/config/DataBase.sql
+++ src/main/java/config/DataBase.sql
@@ -10,3 +10,12 @@ create table if not exists user_table
     national_code varchar default 11,
     phone         varchar default 12
 );
+------------------------------------
+create table if not exists cart_shopping
+(
+    id serial not null unique,
+    user_id integer not null constraint id references user_table,
+    product varchar default 50 not null,
+    price bigint not null
+)
+-------------------------------------
