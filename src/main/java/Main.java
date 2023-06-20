a64438a452f950cf7160c56691d7536dfaecabc6
saber <sbaerm.com@gmail.com>
2023-06-20 13:44:55 +0330

    add shopping cart repositoryi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import ui.Menu;
 
+
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Menu.run();
+
     }
 }
--- src/main/java/repository/ShoppingCartRepository.java
+++ src/main/java/repository/ShoppingCartRepository.java
@@ -3,5 +3,9 @@ package repository;
 import Base.repository.BaseRepository;
 import model.ShoppingCart;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ShoppingCartRepository extends BaseRepository<Integer, ShoppingCart> {
+    public List<ShoppingCart> findAllByUserName(int id) throws SQLException;
 }
