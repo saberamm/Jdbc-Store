88989f34da01ef1a856cda37cb20655c1a33a615
saber <sbaerm.com@gmail.com>
2023-06-22 05:53:36 +0330

    add some field to constant

--- src/main/java/util/Constant.java
+++ src/main/java/util/Constant.java
@@ -11,20 +11,25 @@ public class Constant {
     public static final String WELCOME_MESSAGE="Hi dear ";
     public static final String BAD_ENTRY_FORMAT = "bad entry format!";
     public static final String  ENTER_CREDENTIAL_DATA = "Enter username and password (split by comma): ";
-    public static final String ENTER_SIGNUP_INFO="Enter your information: ";
+    public static final String ENTER_SIGNUP_INFO="Enter your information. ";
     public static final String USER_EXISTS = "this username is taken by another ";
     public static final String NAT_CODE_EXISTS = "this national code is exists ";
     public static final String PHONE_EXISTS = "this phone is exists ";
     public static final String ENTER_NAT_CODE = "Enter your national code: ";
     public static final String ENTER_PHONE = "Enter your phone: ";
+    public static final String ENTER_ID = "Enter the user id: ";
     public static final String ENTER_FIRST_NAME = "Enter your firstname: ";
     public static final String ENTER_LAST_NAME = "Enter your lastname: ";
     public static final String ENTER_PASSWORD = "Enter your password: ";
     public static final String ENTER_USERNAME = "Enter your username: ";
     public static final String REGISTRATION_SUCCESS = "registration was successfully";
+    public static final String UPDATE_SUCCESS = "update was successfully";
+    public static final String PRODUCT_CHOOSE = "choose one of these items";
 
 
     public static final String [] FIRST_MENU={"LOGIN","SIGN UP","EXIT"};
-    public static final String [] DASHBOARD_MENU= {"MY PROFILE","UPDATE PROFILE","DELETE ACCOUNT","SIGN OUT"};
+    public static final String [] DASHBOARD_MENU= {"SHOP","UPDATE PROFILE","DELETE ACCOUNT","SIGN OUT"};
+    public static final String [] CART_MENU= {"SHOPPING","UPDATE CART","DELETE CART","CART PRICE","LAST MENU"};
+    public static final String [] PRODUCT_MENU= {"tv","radio","official shoe","sport shoe","back to last menu"};
 
 }
